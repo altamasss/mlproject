1dd5aa94e5e8230c2639d9b3c8cbd70056b3b715
altamasss <altamas.star@gmail.com>
2023-05-10 11:12:17 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import dill
 #import pickle
-#from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,7 +21,7 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-'''    
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -52,7 +52,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+'''    
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
